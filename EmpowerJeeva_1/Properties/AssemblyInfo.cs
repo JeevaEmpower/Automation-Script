588ce16dc6fe32da5e038a75cb3c033d8639d968
Jeeva Suria Rajah <jeeva.suria.rajah@skyline.be>
2023-06-05 14:34:12 +0800

    Add project files.

---
+++ EmpowerJeeva_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerJeeva_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerJeeva_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("6864D53B-38E2-41FA-87C7-7D1B1DA0F348")]
+[assembly: AssemblyVersion("1.0.0.0")]
